c7de94d2f7846a44ded8f2f7ce07744b92c58816
mahmoudkhlifi <mahmoodkhlifi@outlook.com>
2025-02-14 11:28:43 +0100

    add games

--- game-zone/app/page.tsx
+++ game-zone/app/page.tsx
@@ -1,5 +1,6 @@
-// import Image from "next/image";
+
 "use client"
+// import Image from "next/image";
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/hero";
